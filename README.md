66c1c8200c2f943e8ddcb6174713cc0155c076f7
BolajiOlajide <bolaji.olajide@andela.com>
2018-11-18 04:14:58 +0100

    create guessing game

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ cargo build # build your app and save it in $(pwd)/target/debug/**
 cargo build --release # build your app with optimisations for prod and saves in $(pwd)/target/release/**
 cargo check # just check to see if you haven't done something bad lol much faster than cargo build
 # because it doesn't produce an executable
+cargo new guessing_game --bin # create a rust program with cargo
 ```
+
+Converting this project to a guessing game lol.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+extern crate rand;
+
+use std::io; // library for obtaining user input from user
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world! - Welcome to earth!");
+    println!("Hello, world! - Welcome to earth!"); // macro used to print to the terminal
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop { // loop command is used to create an infinite loop
+        println!("Please input your guess.");
+
+        // variables in Rust are immutable by default
+        // let foo = bar; basically declares foo as a constant (immutable)
+        // let mut foo = bar; makes foo mutable
+        let mut guess = String::new(); // String::new() returns a new instance of a string
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // The & indicates that this argument is a reference, which gives you a way to let multiple
+        // parts of your code access one piece of data without needing to copy that data into memory
+        // multiple times. References are a complex feature, and one of Rust’s major advantages is
+        // how safe and easy it is to use references.
+
+        // References like variables are also immutable by default hence why we write (&mut guess) instead
+        // of (&guess).
+
+        let guess: u32 = match guess.trim().parse() {
+            // convert guess to an unsigned 32-bit integer
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+        println!("")
+    }
 }
